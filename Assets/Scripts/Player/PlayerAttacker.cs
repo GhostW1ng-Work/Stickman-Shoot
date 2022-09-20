3804de46643a445fdd967b7fcd27a297681fcf65
GhostW1ng <workjustghostw1ng@gmail.com>
2022-09-20 15:57:38 +0300

    Create Attack Limit

--- Assets/Scripts/Player/PlayerAttacker.cs
+++ Assets/Scripts/Player/PlayerAttacker.cs
@@ -1,28 +1,28 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerAttacker : MonoBehaviour
 {
     [SerializeField] private Weapon _currentWeapon;
 
     private Animator _animator;
+    private bool _canAttack;
+
+    public event UnityAction Attacked;
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
+        _canAttack = false;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.TryGetComponent(out Enemy enemy))
+        if (collision.gameObject.TryGetComponent(out Enemy enemy) && _canAttack == true)
         {
             StartCoroutine(Attack());
             _currentWeapon.Attack(enemy);
-        }
-
-        if(collision.gameObject.TryGetComponent(out WeaponBox weaponBox))
-        {
-            SetWeapon(weaponBox.Weapon);
         }
     }
 
@@ -36,7 +36,10 @@ public class PlayerAttacker : MonoBehaviour
 
     private void SetWeapon(Weapon weapon)
     {
+        _currentWeapon.gameObject.SetActive(false);
         _currentWeapon = weapon;
+        weapon.gameObject.SetActive(true);
+        _canAttack = true;
     }
 
     private IEnumerator Attack()
@@ -44,5 +47,7 @@ public class PlayerAttacker : MonoBehaviour
         _animator.SetBool("BaseballAttack", true);
         yield return new WaitForSeconds(0.01f);
         _animator.SetBool("BaseballAttack", false);
+        Attacked?.Invoke();
+        _canAttack = false;
     }
 }
--- Assets/Scripts/Weapons/Baseball.cs
+++ Assets/Scripts/Weapons/Baseball.cs
@@ -1,9 +1,31 @@
+using System.Collections;
 using UnityEngine;
 
 public class Baseball : Weapon
 {
-    [SerializeField] private PlayerMover _player;
+    [SerializeField] private PlayerAttacker _player;
     [SerializeField] private float _pushPower;
+
+    private void OnEnable()
+    {
+        _player.Attacked += OnAttacked;
+    }
+
+    private void OnDisable()
+    {
+        _player.Attacked -= OnAttacked;
+    }
+
+    private void OnAttacked()
+    {
+        StartCoroutine(WeaponSetActive());
+    }
+
+    private IEnumerator WeaponSetActive()
+    {
+        yield return new WaitForSeconds(0.5f);
+        gameObject.SetActive(false);
+    }
 
     public override void Attack(Enemy enemy)
     {
