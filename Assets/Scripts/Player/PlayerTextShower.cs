ec31aa13be355189bb230fe91b06ccc5b2491f17
GhostW1ng <workjustghostw1ng@gmail.com>
2022-09-23 14:37:05 +0300

    Create Text Above Head

--- Assets/Scripts/Player/PlayerTextShower.cs
+++ Assets/Scripts/Player/PlayerTextShower.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 public class PlayerTextShower : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
+    [SerializeField] private Player _target;
+    [SerializeField] private Vector3 _offset;
+    [SerializeField] private float _speed;
+
+    private void Start()
+    {
+        transform.position = _target.transform.position + _offset;
+    }
 
     private void Update()
     {
-        transform.LookAt(_camera.transform);
-        transform.rotation = _camera.transform.rotation;
+        transform.position = Vector3.MoveTowards(transform.position, _target.transform.position + _offset, _speed * Time.deltaTime);
     }
 }
  
