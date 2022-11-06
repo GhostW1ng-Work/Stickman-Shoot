b89db34281fe5dbc5bbcf4d2b799c787331da6d1
GhostW1ng <workjustghostw1ng@gmail.com>
2022-11-06 21:49:51 +0300

    Add initialize yandexSDK

--- Assets/Scripts/YandexSDK.cs
+++ Assets/Scripts/YandexSDK.cs
@@ -5,7 +5,11 @@ using UnityEngine.UI;
 
 public class YandexSDK : MonoBehaviour
 {
-    [SerializeField] private InputField _playerDataTextField;
+    [SerializeField]
+    private Text _authorizationStatusText;
+
+    [SerializeField]
+    private Text _personalProfileDataPermissionStatusText;
 
     private void Awake()
     {
@@ -14,26 +18,24 @@ public class YandexSDK : MonoBehaviour
 
     private IEnumerator Start()
     {
+#if !UNITY_EDITOR && UNITY_WEBGL
         yield return YandexGamesSdk.Initialize();
-    }
 
-    public void ShowInterstitialAd()
-    {
-        InterstitialAd.Show();
-    }
+        while (true)
+        {
+            _authorizationStatusText.color = PlayerAccount.IsAuthorized ? Color.green : Color.red;
 
-    public void ShowVideoAd()
-    {
-        VideoAd.Show();
-    }
+            if (PlayerAccount.IsAuthorized)
+                _personalProfileDataPermissionStatusText.color = PlayerAccount.HasPersonalProfileDataPermission ? Color.green : Color.red;
+            else
+                _personalProfileDataPermissionStatusText.color = Color.red;
 
-    public void OnSetPlayerData()
-    {
-        PlayerAccount.SetPlayerData(_playerDataTextField.text);
-    }
-
-    public void OnGetPlayerData()
-    {
-        PlayerAccount.GetPlayerData((data) => _playerDataTextField.text = data);
+            yield return new WaitForSecondsRealtime(0.25f);
+        }
+#else
+        yield break;
+#endif
     }
 }
+
+
