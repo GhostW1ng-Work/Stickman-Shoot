777ce49e6039f7766f22f558847f981f980cc918
GhostW1ng <workjustghostw1ng@gmail.com>
2022-11-10 15:17:36 +0300

    Create Second island Cannon Activator

---
+++ Assets/Scripts/ArenaSwitch/CannonSwitcher.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CannonSwitcher : MonoBehaviour
+{
+    [SerializeField] private EnemyOnArenaCounter _enemyOnArenaCounter;
+    [SerializeField] private CannonActivator _cannonFirstIsland;
+    [SerializeField] private CannonActivator _cannonSecondIsland;
+
+    private void OnEnable()
+    {
+        _enemyOnArenaCounter.EnemiesOnArenaDied += OnEnemiesOnArenaDied;
+    }
+
+    private void OnDisable()
+    {
+        _enemyOnArenaCounter.EnemiesOnArenaDied -= OnEnemiesOnArenaDied;
+    }
+
+    private void OnEnemiesOnArenaDied()
+    {
+        _cannonFirstIsland.gameObject.SetActive(false);
+        _cannonSecondIsland.enabled = true;
+    }
+}
