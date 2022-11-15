0de4d72b7df6aa603160ef79a3dab32517a4a8b3
GhostW1ng <workjustghostw1ng@gmail.com>
2022-11-16 01:40:03 +0300

    Add level saves, edit levels

--- Assets/Scripts/MoverActivator.cs
+++ Assets/Scripts/MoverActivator.cs
@@ -34,6 +34,7 @@ public class MoverActivator : MonoBehaviour
 
         else if(Agava.YandexGames.Device.Type == Agava.YandexGames.DeviceType.Mobile)
         {
+            _desktopMover.enabled = true;
             _mobileMover.enabled = true;
         }
 #else
