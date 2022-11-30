5dbe76d07f32137aa844e9c5b0a553d2561bf718
GhostW1ng <workjustghostw1ng@gmail.com>
2022-11-30 13:53:04 +0300

    Create disable level counter text after start game

--- Assets/Scripts/UI/LevelCounterText.cs
+++ Assets/Scripts/UI/LevelCounterText.cs
@@ -5,9 +5,32 @@ using UnityEngine.SceneManagement;
 public class LevelCounterText : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _levelCounterText;
+    [SerializeField] private GameObject _levelCounterTextGameObject;
+
+    private MoveToStartPanelDisabler _moveToStart;
+
+    private void Awake()
+    {
+        _moveToStart = GetComponentInChildren<MoveToStartPanelDisabler>();
+    }
 
     private void Start()
     {
         _levelCounterText.text = (SceneManager.GetActiveScene().buildIndex + 1).ToString();
+    }
+
+    private void OnEnable()
+    {
+        _moveToStart.AnyKeyPressed += OnAnyKeyPressed;
+    }
+
+    private void OnDisable()
+    {
+        _moveToStart.AnyKeyPressed -= OnAnyKeyPressed;
+    }
+
+    private void OnAnyKeyPressed()
+    {
+        _levelCounterTextGameObject.SetActive(false);
     }
 }
